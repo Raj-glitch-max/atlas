205283f839d32bbc60349e51b9e2c1ba59f22f2a
Raj Patil <rpdinkar92260@gmail.com>
2026-05-21 16:35:35 +0530

    refactor(lab): refactor secrets scanner config (#156)

--- internal/revstatus/statuslist_test.go
+++ internal/revstatus/statuslist_test.go
@@ -161,3 +161,5 @@ func TestSignedSet_SatisfiesContractWhenIgnorant(t *testing.T) {
 	prov := revstatus.NewSignedSetProvider(&k.PublicKey, "list-A")
 	contracttest.Run(t, prov, inst(t, "unknown"), inst(t, "a"), inst(t, "b"))
 }
+
+// checkpoint: refactor(lab): refactor secrets scanner config (#156)
